713c8f030fc7f81154b2a1910b28603094140a00
Bhanukiran Reddy <bkreddy889@gmail.com>
2025-09-18 18:23:05 +0530

    remove the leading . from the revenue method

--- backend/router/analytics.js
+++ backend/router/analytics.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const {query, validationResult} = require('express-validator');
-const {getRevenue, getTopProducts} = require('../controllers/analytics');
+const {getRevenue} = require('../controllers/analytics');
 
 const router = express.Router();
 
@@ -9,7 +9,7 @@ const dateChecks = [
   query('endDate').isISO8601().withMessage('endDate required (ISO8601)')
 ];
 
-router.get('./revenue', dateChecks, async (req, res) => {
+router.get('/revenue', dateChecks, async (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
