c63e83de4fd5c56e908daa508a2ec668486c4a8a
Bhanukiran Reddy <bkreddy889@gmail.com>
2025-09-18 16:44:32 +0530

    defain customer schema

---
+++ backend/models/Customer.js
@@ -0,0 +1,9 @@
+const mongoose =require = require('mongoose');
+
+const CustomerSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    region: { type: String, required: true },
+    type: { type: String, required: true },
+});
+
+module.exports = mongoose.model('Customer', CustomerSchema);
