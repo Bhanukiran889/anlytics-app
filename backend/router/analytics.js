bedf93c202154fdae5ec7d979805f22924e55001
Bhanukiran Reddy <bkreddy889@gmail.com>
2025-09-18 18:02:22 +0530

    create revenue get method

---
+++ backend/router/analytics.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const {query, validationResult} = require('express-validator');
+const {getRevenue, getTopProducts} = require('../controllers/analytics');
+
+const router = express.Router();
+
+const dateChecks = [
+  query('startDate').isISO8601().withMessage('startDate required (ISO8601)'),
+  query('endDate').isISO8601().withMessage('endDate required (ISO8601)')
+];
+
+router.get('./revenue', dateChecks, async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }   
+    getRevenue(req, res, next);
+});
+
+
+
+module.exports = router;
