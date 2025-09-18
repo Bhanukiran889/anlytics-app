b8494bf58a032823bb7554fd69552cb4f25a235d
Bhanukiran Reddy <bkreddy889@gmail.com>
2025-09-18 16:52:16 +0530

    create Sales Schema

---
+++ backend/models/Sale.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+
+const SaleSchema = new mongoose.Schema({
+    customer: { type: mongoose.Schema.Types.ObjectId, ref: 'Customer', required: true },
+    product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+    quantity: { type: Number, default: 1 },
+    totalRevenue: { type: Number, required: true },
+    reportDate: { type: Date, required: true },
+});
