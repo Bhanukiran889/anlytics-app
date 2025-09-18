13b24b14b4adbccddd68f319b3b2f851ec4e2000
Bhanukiran Reddy <bkreddy889@gmail.com>
2025-09-18 16:34:15 +0530

    connent to MongoDB with Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const mongoose = require('mongoose');
 const cors = require('cors');
 const morgan = require('morgan');
-
+const connectDB = require('./config/db');
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(cors());
+
+connectDB();
 
 app.get('/', (req, res) => {
     res.send('API is running...');
