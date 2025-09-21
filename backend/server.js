823744bf9093e354c0dc01851186b33b842be42f
Bhanukiran Reddy <bkreddy889@gmail.com>
2025-09-21 15:09:57 +0530

    update CORS origin links

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,19 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
-app.use(cors());
+
+const corsOptions = {
+  origin: [
+      "https://anlytics-app-six.vercel.app/", // deployed frontend on Vercel
+    "http://localhost:5173"      // local dev frontend
+  ],
+  methods: "GET,POST,PUT,DELETE,OPTIONS",
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
 connectDB();
 
 app.get("/api/health", (req, res) => {
