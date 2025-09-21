d6886575688d80d87f179591fdaca12475bc7c48
Bhanukiran Reddy <bkreddy889@gmail.com>
2025-09-21 15:04:29 +0530

    change import name

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import ProtectedRoute from "./components/ProtectedRoute";
 import Dashboard from "./pages/Dashboard";
-import Login from "./pages/Login.jsx";
+import Login from "./pages/login";
 
 import "./App.css";
 
